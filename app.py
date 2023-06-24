03b7351f668a5272da9154b7b2968878ed530e6d
khaihieu <khaihieupc2@gmail.com>
2023-06-24 18:36:13 +0700

    init

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask,request
+from flask_cors import CORS
+import random
+import json
+import torch
+from model import NeuralNet
+from nltk_utils import bag_of_words, tokenize
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+app = Flask(__name__)
+CORS(app)
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/chat", methods=['POST'])
+def chat():
+    json = request.get_json()
+    question = json.get('question')
+    question = tokenize(question)
+    X = bag_of_words(question, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X).to(device)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+    if prob.item() > 0.75:
+        for intent in intents['intents']:
+            if tag == intent["tag"]:
+                reponse = random.choice(intent['responses'])
+            
+    else:
+        reponse =  "I don't understand"
+    return reponse
